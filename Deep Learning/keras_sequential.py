3254a8d9d38f7fe214885ed2c79486eb23b8b4a4
James Guana <jdpguana@histark.com>
2025-02-19 03:09:18 +0800

    Don't pre-allocate jax so we can avoid OOM

--- Deep Learning/keras_sequential.py
+++ Deep Learning/keras_sequential.py
@@ -40,6 +40,7 @@ from langflow.io import (
 
 os.environ["TF_GPU_ALLOCATOR"]="cuda_malloc_async"
 os.environ["TF_CPP_VMODULE"]="gpu_process_state=10,gpu_cudamallocasync_allocator=10"
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"]="false"
 
 class KerasSequential (Component):
     display_name = "Keras Sequential"
